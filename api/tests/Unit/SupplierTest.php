b923400e5e0dd37d5b0de23e4d391f725fb3e38d
Asafe Ramos <asafe@asaferamos.com>
2019-02-28 10:24:03 -0300

    supplier create test

--- api/tests/Unit/SupplierTest.php
+++ api/tests/Unit/SupplierTest.php
@@ -10,10 +10,13 @@ class SupplierTest extends TestCase
 {
     public function testCreateNewSupplier()
     {
+        $company = \App\Company::find(1);
+
         $data = [
             'name'           => "Supplier One",
             'email'          => "supplier@supplier.com",
-            'monthlypayment' => 300.00
+            'monthlypayment' => 300.00,
+            'company_id'     => $company->id
         ];
 
         $user = factory(\App\User::class)->create();
