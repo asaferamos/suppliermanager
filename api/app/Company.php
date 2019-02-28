0f4f5d62eeec20e53ca1e50adf0115a8e933f28b
Asafe Ramos <asafe@asaferamos.com>
2019-02-28 10:21:19 -0300

    add relationship on models

--- api/app/Company.php
+++ api/app/Company.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $hidden = ["created_at", "updated_at"];
+
+    public function suppliers()
+    {
+        return $this->hasMany(Supplier::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
--- api/app/Supplier.php
+++ api/app/Supplier.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $hidden = ["created_at", "updated_at"];
+
+    public function companies()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
